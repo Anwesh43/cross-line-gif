351c6fd78bfad12451a407b4bb103109e81d65ac
Anwesh43 <anweshthecool0@gmail.com>
2018-10-18 20:37:40 +0530

    adding CLSNode class

--- CrossLineStepGif.js
+++ CrossLineStepGif.js
@@ -25,3 +25,59 @@ class State {
         }
     }
 }
+
+class CLSNode {
+    constructor(i) {
+        this.state = new State()
+        this.i = i
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new CLSNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        const gap : number = w / (nodes + 1)
+        const size : number = gap / 3
+        const sk : number = 1 / factor
+        context.lineWidth = Math.min(w, h) / 60
+        context.lineCap = 'round'
+        context.strokeStyle = '#673AB7'
+        context.save()
+        context.translate(gap * this.i + gap, h/2)
+        for (var j = 0; j < factor; j++) {
+            const sc : number = Math.min(sk, Math.max(0, this.state.scale - sk * j)) * factor
+            context.save()
+            context.rotate(j * Math.PI/2)
+            context.beginPath()
+            context.moveTo(size * sc, 0)
+            context.lineTo(size, 0)
+            context.stroke()
+            context.restore()
+        }
+        context.restore()
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir: number, cb : Function) : CLSNode {
+        var curr : CLSNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
